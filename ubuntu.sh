783c3efa8ecb269a712897529fc909e39bd978c7
GOBMi-ux <89739451+GOBMi-ux@users.noreply.github.com>
2022-03-12 17:23:18 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,57 +1,35 @@
-echo -e "$123\n$123\n" | sudo passwd
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
+rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt-get install firefox -y > /dev/null 2>&1
+echo "===================================="
+echo "Download RDP"
+echo "===================================="
+sudo apt install xrdp -y
 clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3388 &>/dev/null &
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull danchitnis/ubuntu-xrdp
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh
+sudo service xrdp start
 clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-echo "===================================="
-echo "Username : ubuntu"
-echo "Password : ubuntu"
-echo "RDP Address:"
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
+echo "Username root"
+echo "Password you made it"
+echo "More Free VPS akuh.net"
+echo "Keep support akuh.net thank you"
 echo "===================================="
-echo "Don't close this tab to keep RDP running"
-echo "Wait to finish bot and next open RDC to connect"
-echo "===================================="
-echo "===================================="
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:20.04 > /dev/null 2>&1
-b='\033[1m'
-r='\E[31m'
-g='\E[32m'
-c='\E[36m'
-endc='\E[0m'
-enda='\033[0m'
-# Branding
-
-printf """$c$b
- 
-██╗     ██╗███╗   ██╗ ██████╗  ██████╗  █████╗ ██╗  ██╗ ██████╗ ███████╗████████╗██╗███╗   ██╗ ██████╗ 
-██║     ██║████╗  ██║██╔════╝ ██╔════╝ ██╔══██╗██║  ██║██╔═══██╗██╔════╝╚══██╔══╝██║████╗  ██║██╔════╝ 
-██║     ██║██╔██╗ ██║██║  ███╗██║  ███╗███████║███████║██║   ██║███████╗   ██║   ██║██╔██╗ ██║██║  ███╗
-██║     ██║██║╚██╗██║██║   ██║██║   ██║██╔══██║██╔══██║██║   ██║╚════██║   ██║   ██║██║╚██╗██║██║   ██║
-███████╗██║██║ ╚████║╚██████╔╝╚██████╔╝██║  ██║██║  ██║╚██████╔╝███████║   ██║   ██║██║ ╚████║╚██████╔╝
-╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝
-    $r  Support YT Channel-> Aank is ME © 2022 $c https://aank.me/Youtube 
-          
-$endc$enda""";
+sleep 43200
